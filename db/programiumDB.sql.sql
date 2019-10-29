9731aaa2f6d9d0c5ab2e32bfebc7bcbd11d8ee09
juancarlord <juancagomezmendez@gmail.com>
2019-10-29 08:49:11 -0500

    creado la base de datos temporal para la pagina

---
+++ db/programiumDB.sql.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) UNIQUE NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `ocupation` varchar(255) NOT NULL,
+  `isProfessor` int NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `reason` varchar(255) NOT NULL
+);
+
+CREATE TABLE `admin` (
+  `id` int PRIMARY KEY,
+  `username` varchar(255) NOT NULL,
+  `fullName` varchar(255) NOT NULL
+);
